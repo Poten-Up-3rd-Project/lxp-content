ec5118959b9c10d6219f37c693616e81df8356a8
minyong96 <aldud159357@gmail.com>
2025-12-21 13:40:54 +0900

    chore: setup

---
+++ src/test/java/com/lxp/content/ContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxp.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
