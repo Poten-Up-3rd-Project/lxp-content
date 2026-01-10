2e3ea68217e4c4d2fbbf5213e754da5b65977b60
minyong96 <aldud159357@gmail.com>
2026-01-10 23:30:36 +0900

    fix: apply RequestMapping

--- src/main/java/com/lxp/content/course/infra/web/internal/CourseInternalController.java
+++ src/main/java/com/lxp/content/course/infra/web/internal/CourseInternalController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api-v1/internal/courses")
+@RestController
+@RequestMapping("/api-v1/internal/courses")
 @RequiredArgsConstructor
 public class CourseInternalController {
 
