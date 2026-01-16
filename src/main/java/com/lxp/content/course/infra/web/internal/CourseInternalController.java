f36fb3888c5678f354a1807be624b0f17baae87c
minyong96 <aldud159357@gmail.com>
2026-01-16 16:53:28 +0900

    Minyoung/feat/api (#46)

--- src/main/java/com/lxp/content/course/infra/web/internal/CourseInternalController.java
+++ src/main/java/com/lxp/content/course/infra/web/internal/CourseInternalController.java
@@ -17,11 +17,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/internal/api-v1/courses")
+@RestController("/api-v1/internal/courses")
 @RequiredArgsConstructor
 public class CourseInternalController {
 
